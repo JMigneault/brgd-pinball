d67ca25380d08f850b314227d49097d481812b18
Sophia Tu <sophiatu217@gmail.com>
2021-03-06 15:37:17 -0500

    Added Points and Lives UI

--- Assets/Scripts/LivesTracker.cs
+++ Assets/Scripts/LivesTracker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LivesTracker : MonoBehaviour
 {
@@ -26,5 +27,9 @@ public class LivesTracker : MonoBehaviour
         return remainingLives;
 
     }
-    
+
+    public int GetLives() {
+        return remainingLives;
+    }
+
 }
