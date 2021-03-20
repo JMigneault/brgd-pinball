dc0018826e88027252b74dfceb7c165c904b4dd9
rickydaricky <zricky2000@gmail.com>
2021-03-20 12:44:44 -0700

    found springer

--- Assets/Scripts/Springer.cs
+++ Assets/Scripts/Springer.cs
@@ -7,11 +7,13 @@ public class Springer : MonoBehaviour
 {
 
     public float launchForce;
+    public float downTime, pressTime = 0;
 
     public GameObject pinball;
     private Rigidbody2D pinballBody;
+    private bool triggered = false;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +25,46 @@ public class Springer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (triggered)
+        {
+            if (Input.GetKeyDown("w"))
+            {
+                downTime = Time.time;
+                StartCoroutine(MoveDown());
+            }
+
+            if (Input.GetKeyUp("w"))
+            {
+                StopCoroutine(MoveDown());
+                pressTime = Time.time - downTime;
+                pinballBody.AddForce(Vector3.up * launchForce * pressTime * 0.3f, ForceMode2D.Impulse);
+                transform.localPosition = new Vector3(2.5f, -4.475f, 1f);
+                transform.localScale = new Vector3(0.8f, 1f, 1f);
+                triggered = false;
+            }
+        } 
+       
     }
 
 
-    void OnTriggerStay2D(Collider2D collidedObject)
+    void OnTriggerEnter2D(Collider2D collidedObject)
     {
         if (collidedObject.gameObject.tag == "Pinball" && Input.GetKey("w")) {
             pinballBody.AddForce(Vector3.up * launchForce, ForceMode2D.Impulse);
         }
+    }
+
+    void OnTriggerExit2D(Collider2D collidedObject)
+    {
+        triggered = false;
+    }
+
+
+    IEnumerator MoveDown()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 0.2f, transform.localPosition.z);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - 0.2f, transform.localScale.z);
+        yield return new WaitForSeconds(0.2f);
     }
 
 }
