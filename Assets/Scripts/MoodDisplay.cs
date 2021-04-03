9e8733412bf746483d001b82887ed1d29b875190
rickydaricky <zricky2000@gmail.com>
2021-04-03 12:04:12 -0700

    added the sprite mood changes

--- Assets/Scripts/MoodDisplay.cs
+++ Assets/Scripts/MoodDisplay.cs
@@ -7,6 +7,12 @@ public class MoodDisplay : MonoBehaviour
 {
     public PointTracker tracker;
     public Text mood_text;
+    public GameObject pet;
+
+    public Sprite psyched;
+    public Sprite meh;
+    public Sprite bummed;
+    public Sprite happy;
 
     // Start is called before the first frame update
     void Start()
@@ -26,15 +32,19 @@ public class MoodDisplay : MonoBehaviour
         // set mood
         if (points < 5) {
             mood_text.text = "Mood: Bummed";
+            pet.GetComponent<SpriteRenderer>().sprite = bummed;
         }
         else if (points < 10) {
             mood_text.text = "Mood: Meh";
+            pet.GetComponent<SpriteRenderer>().sprite = meh;
         }
         else if (points< 15) {
             mood_text.text = "Mood: Happy";
+            pet.GetComponent<SpriteRenderer>().sprite = happy;
         }
         else {
             mood_text.text = "Mood: Psyched";
+            pet.GetComponent<SpriteRenderer>().sprite = psyched;
         }
 
         // display mood
